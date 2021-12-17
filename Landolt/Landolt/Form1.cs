7cf19664f4e04992cef34e5d72fb60f0ad7bfcde
MegaMozg86 <alex.tim1986@gmail.com>
2021-12-18 02:29:15 +0700

    final

--- Landolt/Landolt/Form1.cs
+++ Landolt/Landolt/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Landolt
 {
@@ -23,16 +24,22 @@ namespace Landolt
         int seconds = 0;// время в секундах
         int timeLeft = 0;
 
+        Report report;
+
         Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
-                        
-            this.Height = button1.Location.Y + 150;            
+
+            this.Height = panel1.Location.Y + panel1.Height + 50;            
+            this.Width = button1.Location.X + 200;
+
+            button2.Enabled = false;
         }
 
         void SetMatrix()
         {
+            report = new Report();
             panel1.Controls.Clear();
 
             ctrls = new List<List<UserControl1>>();
@@ -67,6 +74,45 @@ namespace Landolt
             panel2.BackgroundImage = Image.FromFile("./Images/" + "_" + targetType.ToString() + ".png");
         }
 
+        void GetReport()
+        {
+            int count = 0;
+            // проход по матрице
+            for (int r = 0; r < rows; ++r)
+            {
+                for (int c = 0; c < cols; ++c)
+                {
+                    count++;
+                    UserControl1 u = ctrls[r][c];
+                    if (u.Type == targetType)
+                        report.n++;
+
+                    if (u.Checked)
+                    {
+                        report.M++;
+                        if (u.Type == targetType)
+                        {
+                            report.S++;
+                            report.N = count;
+                            report.C = r + 1;
+                        }
+                        // неверно отмеченный
+                        if (u.Type != targetType)
+                            report.O++;
+                    }
+                    else
+                    {
+                        // пропущенный
+                        if (u.Type == targetType)
+                            report.P++;
+                    }
+                }
+            }
+
+            textBox4.Text = report.ToString();
+            button2.Enabled = true;
+        }
+
         void Finish()
         {
             timer.Stop();
@@ -92,12 +138,24 @@ namespace Landolt
                     }
                 }
             }
+
+            GetReport();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             if(!started)
             {
+                if(textBox1.Text == string.Empty ||
+                    textBox2.Text == string.Empty ||
+                    textBox3.Text == string.Empty)
+                {
+                    MessageBox.Show("Заполните все поля!", "Внимание!");
+                    return;
+                }
+
+                textBox4.Text = string.Empty;
+                button2.Enabled = false;
                 SetMatrix();
                 seconds = ((int)numericUpDown2.Value) * 60;
                 timeLeft = seconds;
@@ -122,10 +180,40 @@ namespace Landolt
 
         private void Timer_Tick(object sender, EventArgs e)
         {
+            report.t += 1;
             timeLeft -= 1;
             ShowTimeLeft(timeLeft);
-            if(timeLeft == 0)
+            if (timeLeft == 0)
+            {
+                MessageBox.Show("Время вышло, тест завершен!", "Внимание!");
                 Finish();
+                label3.Text = "Осталось времени:";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "txt files (*.txt)|*.txt";
+                dialog.FileName = string.Format("{0} {1} {2} {3}",
+                    textBox1.Text, textBox2.Text, textBox3.Text, DateTime.Now.ToString("dd-MM-yyyy-HH-mm"));
+                var r = dialog.ShowDialog();
+                if (r == DialogResult.OK)
+                {
+                    string content = string.Format("{0} {1} {2} {3}",
+                    textBox1.Text, textBox2.Text, textBox3.Text, DateTime.Now.ToString("dd-MM-yyyy-HH-mm"));
+                    content += Environment.NewLine;
+                    content += report.ToString();
+                    File.WriteAllText(dialog.FileName, content);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!");
+            }
         }
     }
 }
---
+++ Landolt/Landolt/Report.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Landolt
+{
+    public class Report
+    {
+        /// <summary>
+        /// затраченное время
+        /// </summary>
+        public double t { get; set; } = 0; // 
+        /// <summary>
+        /// кол-во символов, которые нужно было вычеркнуть
+        /// </summary>
+        public double n { get; set; } = 0; // 
+        /// <summary>
+        /// кол-во всех вычеркнутых символов
+        /// </summary>
+        public double M { get; set; } = 0; // 
+        /// <summary>
+        /// 
+        /// кол-во верно вычеркнутых символов (зеленые)
+        /// </summary>
+        public double S { get; set; } = 0; // 
+        /// <summary>
+        /// кол-во пропущенных (желтые)
+        /// </summary>
+        public double P { get; set; } = 0; // 
+        /// <summary>
+        /// кол-во ошибочных символов (красные)
+        /// </summary>
+        public double O { get; set; } = 0; // 
+        /// <summary>
+        /// кол-во просмотренных до последнего
+        /// </summary>
+        public double N { get; set; } = 0; // 
+        /// <summary>
+        /// кол-во строк просмотренных до последнего просмотренного
+        /// </summary>
+        public double C { get; set; } = 0; // 
+
+        ///////////////////////////////////////////////////
+
+        public double A { get { return N / t; } }
+        public double T1 { get { return M / n; } }
+        public double T2 { get { return S / n; } }
+        public double T3 { get { return (M - O) / (M + P); } }
+        public double E { get { return N * T2; } }
+        public double Au { get { return (N / t) *((M - (O + P)) / n); } }
+        public double K { get { return ((M - O) * 100) / n; } }
+        public double Ku 
+        { 
+            get 
+            {
+                if (M == n)
+                    return C * (C / (P + O + 1));
+                else
+                    return C * (C / (P + O));
+            } 
+        }
+        public double V { get { return 0.5936 * N; } }
+        public double Q { get { return (V - 2.807 * (P + O)) / t; } }
+
+        public override string ToString()
+        {
+            string s = string.Empty;
+            s += "A = " + A + Environment.NewLine;
+            s += "T1 = " + T1 + Environment.NewLine;
+            s += "T2 = " + T2 + Environment.NewLine;
+            s += "T3 = " + T3 + Environment.NewLine;
+            s += "E = " + E + Environment.NewLine;
+            s += "Au = " + Au + Environment.NewLine;
+            s += "K = " + K + Environment.NewLine;
+            s += "Ku = " + Ku + Environment.NewLine;
+            s += "V = " + V + Environment.NewLine;
+            s += "Q = " + Q + Environment.NewLine;
+            return s;
+        }
+
+    }
+}
